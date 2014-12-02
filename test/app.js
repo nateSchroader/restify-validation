13acc732a58ac98c646645d7755d29b7b8262a58
Nate Schroader <nathan@akirix.com>
2014-12-02 15:15:34 -0700

    init

---
+++ test/app.js
@@ -0,0 +1,28 @@
+var restify = require( 'restify' );
+var restifyValidation = require( '../index.js' );
+var server = restify.createServer( {
+    name: 'Test API',
+    version: '1.0.0'
+} );
+
+server.use( restify.acceptParser( server.acceptable ) );
+server.use( restify.queryParser() );
+server.use( restify.bodyParser() );
+server.use( restify.authorizationParser() );
+server.use( restify.CORS() );
+server.use( restify.fullResponse() );
+server.use( restifyValidation() );
+
+restify.CORS.ALLOW_HEADERS.push( 'authorization' );
+
+server.listen( 3000, function(){
+} );
+
+server.post( '/test', function( req, res, next ){
+
+    req.assert( 'name', 'isString', {} );
+
+    res.send( 200, {} );
+
+    return next();
+} );
